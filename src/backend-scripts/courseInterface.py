27de1fe53958d24d92769fbe04112a4064b2c2ba
Matthew Boisvert <mboisver@ucsc.edu>
2021-01-16 15:02:25 -0800

    Added departments to organize nodes

--- src/backend-scripts/courseInterface.py
+++ src/backend-scripts/courseInterface.py
@@ -6,12 +6,12 @@ from connectDatabase import DatabaseEngine
 
 
 
-def fetchCourses() -> []:
+def fetchCourses(numQuartersAgo):
   TERM_LIST_URL = "https://andromeda.miragespace.net/slugsurvival/data/fetch/terms.json"
   TERM_INFO_URL = "https://andromeda.miragespace.net/slugsurvival/data/fetch/terms/"
   COURSE_INFO_URL = "https://andromeda.miragespace.net/slugsurvival/data/fetch/courses/"
 
-  ucsc_term = getCurrentQuarter(TERM_LIST_URL)
+  ucsc_term = getQuarter(TERM_LIST_URL, numQuartersAgo)
   term_id = ucsc_term["code"]
   quarter = ucsc_term["name"].split()[1] + " " + ucsc_term["name"].split()[0]
 
@@ -19,7 +19,7 @@ def fetchCourses() -> []:
   course_data = fetcherHelper.sendGetRequest(COURSE_INFO_URL+str(term_id)+".json")
 
 
-  all_courses = []
+  all_courses = {}
   # Getting the data available from the term API (name, class time)
   for course_prefix in term_data:
     for course in term_data[course_prefix]:
@@ -30,21 +30,15 @@ def fetchCourses() -> []:
       new_course.name = full_name
 
       class_time = course["loct"][0]["t"]
-      location = course["loct"][0]["loc"]
       new_course.geCodes = course_data[str(new_course.id)]["ge"]
       prerequisites = course_data[str(new_course.id)]["re"]
       if prerequisites is not None:
         new_course.prerequisites = re.findall("[A-Z]{1,5} [0-9A-Z]{1,3}", prerequisites)
 
-      if (location == "Remote Instruction" or location == "Online") and class_time == None:
-        new_course.time = None
-        all_courses.append(new_course)
-        continue
-
       if(class_time != None and class_time != False):
         try:
           new_course.time = (class_time["time"]["start"], class_time["time"]["end"])
-          all_courses.append(new_course)
+          all_courses[short_name] = new_course
         except Exception as e:
           pass
 
@@ -64,30 +58,59 @@ Returns JSON of the form:
 }
 """
 
-def getCurrentQuarter(api_url: str) -> json:
+def getQuarter(api_url: str, numQuartersAgo) -> json:
     data = fetcherHelper.sendGetRequest(api_url)
-    return data[0]
+    return data[numQuartersAgo]
 
 def fillCourseJson(fileName):
   data = {}
   data["nodes"] = []
   data["links"] = []
-  courses = fetchCourses()
+  courses = []
+  courseMap = fetchCourses(0)
+  courseMap.update(fetchCourses(1))
+  courseMap.update(fetchCourses(3))
+  courses = [courseMap[key] for key in courseMap]
+  addedDepartments = set()
+  extraCourses = set()
 
   for course in courses:
     id = course.name.split(" - ")[0]
+    department = id.split(" ")[0]
+
+    if department not in addedDepartments:
+        data["nodes"].append({
+          "id": department,
+          "group": abs(hash(department))
+        })
+        addedDepartments.add(department)
+
     data["nodes"].append({
       "id": id,
-      "group": abs(hash(course.name.split(" ")[0]) % 50)
+      "group": abs(hash(department))
     })
+
+    data["links"].append({
+      "source": id,
+      "target": department,
+      "value": 0
+    })
+
     for prereq in course.prerequisites:
-      classesWithID = [item for item in data["nodes"] if item['id'] == prereq]
-      if len(classesWithID) > 0:      
-        data["links"].append({
-          "source": id,
-          "target": prereq,
-          "value": 1
+      if prereq not in courseMap and prereq not in extraCourses:
+        data["nodes"].append({
+          "id": prereq,
+          "group": abs(hash(prereq.split(" ")[0]))
         })
+        extraCourses.add(prereq)
+
+      data["links"].append({
+        "source": prereq,
+        "target": id,
+        "value": 1
+      })
+      
+
 
   with open(fileName, 'w') as outfile:
       json.dump(data, outfile)
@@ -114,4 +137,4 @@ def getCourses():
   databaseEngine = DatabaseEngine()
   return databaseEngine.getCourses()
 
-fillCourseJson('../frontend/test.json')
+fillCourseJson('../frontend/data.json')
